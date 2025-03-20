83a6231cb05fd6327fe340519fef14c14ad3d778
TerenceP1 <158529801+TerenceP1@users.noreply.github.com>
2025-03-20 16:58:39 -0400

    Update views.py

--- example/views.py
+++ example/views.py
@@ -8,10 +8,13 @@ import json
 import httpx
 @csrf_exempt
 def index(request):
-    now = datetime.now()
-    if request.method!="POST":
-        return HttpResponse("Sorry, only POST is supported. If you are a regular user looking at this page, no worries! Just close this page and continue on!")
-    res={}
-    res["success"]=True
-    res["body"]=httpx.get("https://google.com").text
-    return HttpResponse(json.dumps(res, sort_keys=True, indent=2))
+    try:
+        now = datetime.now()
+        if request.method!="POST":
+            return HttpResponse("Sorry, only POST is supported. If you are a regular user looking at this page, no worries! Just close this page and continue on!")
+        res={}
+        res["success"]=True
+        res["body"]=httpx.get("https://google.com").text
+        return HttpResponse(json.dumps(res, sort_keys=True, indent=2))
+    except BaseException as e:
+        return HttpResponse(f"Internal error! Error message:{e}")
