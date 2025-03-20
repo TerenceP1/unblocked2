bc34eb02bd4aec72defbfaac7328f1c11a98a32e
TerenceP1 <158529801+TerenceP1@users.noreply.github.com>
2025-03-20 15:16:31 -0400

    Update views.py

--- example/views.py
+++ example/views.py
@@ -5,6 +5,8 @@ from django.http import HttpResponse
 
 def index(request):
     now = datetime.now()
+    if request.method!="POST":
+        return HttpResponse("Sorry, only POST is supported. If you are a regular user looking at this page, no worries! Just close this page and continue on!")
     html = f'''
     <html>
         <body>
