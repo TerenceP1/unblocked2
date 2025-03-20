85f9cfd6aed70781ef30733c6c75618193999b1a
TerenceP1 <158529801+TerenceP1@users.noreply.github.com>
2025-03-20 18:02:25 -0400

    Update views.py

--- example/views.py
+++ example/views.py
@@ -16,6 +16,17 @@ def index(request):
         if request.method!="POST":
             return HttpResponse("Sorry, only POST is supported. If you are a regular user looking at this page, no worries! Just close this page and continue on!")
         res={}
+        templ={
+            "url": str,
+            "method": str,
+            "headers": dict,
+            "body": (str)
+        }
+        for key, expected_type in required_keys.items():
+            if key not in data:
+                raise ValueError(f"Missing required key: {key}")
+            if not isinstance(data[key], expected_type):
+                raise TypeError(f"Invalid type for {key}: Expected {expected_type}, got {type(data[key])}")
         res["success"]=True
         req=httpx.get("https://google.com")
         res["body"]=base64.b64encode(req.content).decode('utf-8')
@@ -24,4 +35,4 @@ def index(request):
         rsp=HttpResponse(json.dumps(res),content_type="application/json")
         return rsp
     except BaseException as e:
-        return HttpResponse(f"Internal error! Error message:{e}",status=500)
+        return HttpResponse(f"Internal error! Error message: {e}",status=500)
