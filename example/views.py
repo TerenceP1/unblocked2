52684ee82761183becc587ef7ba2f64adc76bf01
TerenceP1 <158529801+TerenceP1@users.noreply.github.com>
2025-03-20 16:37:55 -0400

    Update views.py

--- example/views.py
+++ example/views.py
@@ -3,16 +3,15 @@ from datetime import datetime
 
 from django.http import HttpResponse
 
+import json
+
+import httpx
+
 def index(request):
     now = datetime.now()
     if request.method!="POST":
         return HttpResponse("Sorry, only POST is supported. If you are a regular user looking at this page, no worries! Just close this page and continue on!")
-    html = f'''
-    <html>
-        <body>
-            <h1>Hello from Vercel!</h1>
-            <p>The current time is { now }.</p>
-        </body>
-    </html>
-    '''
-    return HttpResponse(html)
+    res={}
+    res["success"]=True
+    res["body"]=httpx.get("https://google.com").text
+    return HttpResponse(json.dumps(res, sort_keys=True, indent=2))
